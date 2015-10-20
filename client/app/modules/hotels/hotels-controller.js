09b34ac233798840f908f78b5cbc5f33f29ed730
Mark Bevels <mark.bevels@gmail.com>
2015-10-20 14:08:41 +0100

    simple result filtering added

--- client/app/modules/hotels/hotels-controller.js
+++ client/app/modules/hotels/hotels-controller.js
@@ -2,34 +2,15 @@ angular
 	.module("app.modules.hotels")
 	.controller("HotelsController", HotelsController);
 
-function HotelsController() {
-	console.log(this.hotels);
-	this.hotels = [ {
-		"Distance": 2.292091742299839,
-		"EstablishmentId": 374814,
-		"EstablishmentType": "Hotel",
-		"Location": "Paris",
-		"MinCost": 1402,
-		"Name": "Le Littre",
-		"Stars": 4,
-		"UserRating": 8.4,
-		"UserRatingTitle": "Great",
-		"UserRatingCount": 23,
-		"ImageUrl": "https://i.t-rp.co.uk/ei/3/7/4/8/1/4/0_400_400.jpg",
-		"ThumbnailUrl": "https://i.t-rp.co.uk/ei/3/7/4/8/1/4/0_80_80.jpg"
-	},
-		{
-			"Distance": 5.0292345498310445,
-			"EstablishmentId": 118244,
-			"EstablishmentType": "Hotel",
-			"Location": "Paris",
-			"MinCost": 1775.76,
-			"Name": "Hotel Plaza Tour Eiffel",
-			"Stars": 4,
-			"UserRating": 8.5,
-			"UserRatingTitle": "Excellent",
-			"UserRatingCount": 96,
-			"ImageUrl": "https://i.t-rp.co.uk/ei/1/1/8/2/4/4/0_400_400.jpg",
-			"ThumbnailUrl": "https://i.t-rp.co.uk/ei/1/1/8/2/4/4/0_80_80.jpg"
-		}];
+function HotelsController($http) {
+	var vm = this;
+	$http({
+		method: 'GET',
+		url: '/app/data/hotels.json'
+	}).then(function successCallback(response) {
+		vm.hotels = response.data.Establishments;
+	}, function errorCallback(response) {
+		// called asynchronously if an error occurs
+		// or server returns response with an error status.
+	});
 }
