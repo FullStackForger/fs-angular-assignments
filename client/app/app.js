884a6fc78f70878093bb7f9fda52835b00345550
Mark Bevels <mark.bevels@gmail.com>
2015-10-20 10:44:00 +0100

    refactoring app.js

--- client/app/app.js
+++ client/app/app.js
@@ -2,17 +2,18 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.views.view1',
-  'myApp.views.view2',
-  'myApp.version'
+	'ngRoute',
+	'myApp.views.view2',
+	'myApp.views.view2',
+	'myApp.modules.hotels',
+	'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/', {
-          templateUrl: 'app/main.html'
-        })
-      .otherwise({
-          redirectTo: '/'
-      });
-}]);
+	config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'app/main.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	}]);
