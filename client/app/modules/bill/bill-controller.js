4134ef7f8854ff56d83e3e467470fafc33543281
Mark Bevels <mark.bevels@gmail.com>
2015-10-22 16:01:08 +0100

    bill data parsing

--- client/app/modules/bill/bill-controller.js
+++ client/app/modules/bill/bill-controller.js
@@ -6,11 +6,18 @@ function BillController($http) {
 	var vm = this;
 
 	$http
-		//.get('app/data/bill.json')
-		.get('https://still-scrubland-9880.herokuapp.com/bill.json')
-		.then(function (response) {
-			vm.bill = response.data;
-		}, function (error) {
+		.get('app/modules/bill/data/bill.json')
+		//.get('https://still-scrubland-9880.herokuapp.com/bill.json')
+		.then(parseBill, function (error) {
 			vm.errorMessage = "Bill not available. Please contact SKY for details."
 		});
+
+	function parseBill(response) {
+		vm.bill = response.data;
+		vm.statement = vm.bill.statement;
+		vm.total = vm.bill.total;
+		vm.package = vm.bill.package;
+		vm.callCharges = vm.bill.callCharges;
+		vm.skyStore = vm.bill.skyStore;
+	}
 }
